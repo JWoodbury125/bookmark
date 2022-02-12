50d17ebf6bc66f3d5b60eb5754c0d6617958e294
Jennifer Woodbury <jenann125@yahoo.com>
2022-02-11 22:04:10 -0500

    Workshop continued

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const express = require ('express')
 const { request } = require('express')
 const app = express()
 
-
-app.get('/', async (req, res, next) =>{
+app.get('/', (req, res) => res.redirect('/bookmarks'))
+app.get('/bookmarks', async (req, res, next) =>{
     try{
         const data = await Bookmark.findAll()
-        const html = `<html><body>${data.map(val => `<ul>${val.name} <a href='/categories/${val.category}'> ${val.category}</a></ul>`).join('')}</body></html>`
+        const html = `<html><h1>Acme Bookmarks</h1><body>${data.map(val => `<ul>${val.name} <a href='/categories/${val.category}'> ${val.category}</a></ul>`).join('')}</body></html>`
         res.send(html)
     }
     catch(ex){
@@ -24,11 +24,17 @@ app.get('/categories/:category', async(req, res, next) => {
             where: { category }
         })
         const html = data.map(val => `<ul>${val.name}</ul>`).join('')
-        res.send(html)
+        res.send(`<html><a href='/bookmarks'> Previous>></a>
+                    <h1> Acme Bookmarks ${ req.params.category } </h1>
+                    <div>
+                        ${html}
+                    </div>
+                  </html>`)
     }
     catch(ex){
         next(ex)
     }
 })
+
 
 app.listen(3000,() => console.log('Connected'))
